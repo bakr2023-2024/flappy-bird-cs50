87a4792a664713446864d123e428304edfc49255
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-28 00:47:09 +0300

    feat: apply gravity to bird and anti-gravity using space

--- Bird.lua
+++ Bird.lua
@@ -1,12 +1,20 @@
 Bird = Class({})
+local GRAVITY = 980
 function Bird:init()
 	self.image = love.graphics.newImage("bird.png")
 	self.width = self.image:getWidth()
 	self.height = self.image:getHeight()
 	self.x = HVW - self.width / 2
 	self.y = HVH - self.height / 2
+	self.dy = 0
 end
-function Bird:update(dt) end
+function Bird:update(dt)
+	self.dy = self.dy + GRAVITY * dt
+	if love.keyboard.wasPressed("space") then
+		self.dy = -300
+	end
+	self.y = self.y + self.dy * dt
+end
 function Bird:render()
 	love.graphics.draw(self.image, self.x, self.y)
 end
--- main.lua
+++ main.lua
@@ -21,13 +21,24 @@ function love.load()
 	background = love.graphics.newImage("background.png")
 	ground = love.graphics.newImage("ground.png")
 	bird = Bird()
-
+	love.keyboard.keysPressed = {}
 	love.window.setMode(WW, WH, { resizable = false, vsync = true, fullscreen = false })
 	push:setupScreen(VW, VH, WW, WH)
 end
 function love.update(dt)
 	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
 	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VW
+	bird:update(dt)
+	love.keyboard.keysPressed = {}
+end
+function love.keypressed(key)
+	love.keyboard.keysPressed[key] = true
+	if key == "escape" then
+		love.event.quit()
+	end
+end
+function love.keyboard.wasPressed(key)
+	return love.keyboard.keysPressed[key] or false
 end
 function love.draw()
 	push:start()
