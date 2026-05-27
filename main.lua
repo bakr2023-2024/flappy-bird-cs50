a239e55398134b711bd01672cfa0001e07238ef9
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-28 00:02:02 +0300

    feat: implement infinite scrolling for background and ground

--- main.lua
+++ main.lua
@@ -4,8 +4,12 @@ VW = 512
 VH = 288
 HVW = VW / 2
 HVH = VH / 2
+local backgroundScroll = 0
+local groundScroll = 0
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+local BACKGROUND_LOOPING_POINT = 413
 push = require("push")
-
 function love.load()
 	love.window.setTitle("Fifty Bird")
 	love.graphics.setDefaultFilter("nearest", "nearest")
@@ -14,9 +18,13 @@ function love.load()
 	love.window.setMode(WW, WH, { resizable = false, vsync = true, fullscreen = false })
 	push:setupScreen(VW, VH, WW, WH)
 end
+function love.update(dt)
+	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VW
+end
 function love.draw()
 	push:start()
-	love.graphics.draw(background, 0, 0)
-	love.graphics.draw(ground, 0, VH - 16)
+	love.graphics.draw(background, -backgroundScroll, 0)
+	love.graphics.draw(ground, -groundScroll, VH - 16)
 	push:finish()
 end
