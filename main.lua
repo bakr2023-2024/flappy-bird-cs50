6bb9cf8ab0b1f24463e2b51ac0f743f767e5ecf0
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-28 18:59:03 +0300

    refactor: main to use StateMachine and transition between states using change

--- main.lua
+++ main.lua
@@ -4,55 +4,58 @@ VW = 512
 VH = 288
 HVW = VW / 2
 HVH = VH / 2
+
 push = require("push")
 Class = require("class")
+
 require("Bird")
 require("Pipe")
 require("PipePair")
+
+require("StateMachine")
+require("states.BaseState")
+require("states.PlayState")
+require("states.TitleState")
+
 local backgroundScroll = 0
 local groundScroll = 0
 -- parallex effect as ground is closer than background so ground scrolls faster than background
 local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
-local pipePairs = {}
-local spawnTimer = 0
 local BACKGROUND_LOOPING_POINT = 413
 local GROUND_LOOPING_POINT = 514
-local prevY = -PIPE_HEIGHT + math.random(80) + 20
 local scrolling = true
 function love.load()
 	love.window.setTitle("Fifty Bird")
 	love.graphics.setDefaultFilter("nearest", "nearest")
 	math.randomseed(os.time())
+
+	smallFont = love.graphics.newFont("font.ttf", 8)
+	mediumFont = love.graphics.newFont("flappy.ttf", 14)
+	flappyFont = love.graphics.newFont("flappy.ttf", 28)
+	hugeFont = love.graphics.newFont("flappy.ttf", 56)
+	love.graphics.setFont(flappyFont)
+
 	background = love.graphics.newImage("background.png")
 	ground = love.graphics.newImage("ground.png")
-	bird = Bird()
+
+	gsm = StateMachine({
+		["title"] = function()
+			return TitleState()
+		end,
+		["play"] = function()
+			return PlayState()
+		end,
+	})
 	love.keyboard.keysPressed = {}
+
 	love.window.setMode(WW, WH, { resizable = false, vsync = true, fullscreen = false })
 	push:setupScreen(VW, VH, WW, WH)
 end
 function love.update(dt)
-	if scrolling then
-		backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-		groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
-		spawnTimer = spawnTimer + dt
-		if spawnTimer > 2 then
-			local y = math.max(math.min(prevY + math.random(-20, 20), VH - GAP_HEIGHT - PIPE_HEIGHT), -PIPE_HEIGHT + 10)
-			prevY = y
-			table.insert(pipePairs, PipePair(y))
-			spawnTimer = 0
-		end
-		bird:update(dt)
-		for i = #pipePairs, 1, -1 do
-			pipePairs[i]:update(dt)
-			if bird:collides(pipePairs[i].upper) or bird:collides(pipePairs[i].lower) then
-				scrolling = false
-			end
-			if pipePairs[i].remove then
-				table.remove(pipePairs, i)
-			end
-		end
-	end
+	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
+	gsm:update(dt)
 	love.keyboard.keysPressed = {}
 end
 function love.keypressed(key)
@@ -68,9 +71,6 @@ function love.draw()
 	push:start()
 	love.graphics.draw(background, -backgroundScroll, 0)
 	love.graphics.draw(ground, -groundScroll, VH - 16)
-	bird:render()
-	for i, pair in ipairs(pipePairs) do
-		pair:render()
-	end
+	gsm:render()
 	push:finish()
 end
