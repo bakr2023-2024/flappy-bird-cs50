b6e73e007b5fe2d5c99e6fc9dcb046c15b0b381e
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-28 18:42:12 +0300

    refactor: add StateMachine class

---
+++ StateMachine.lua
@@ -0,0 +1,18 @@
+StateMachine = Class({})
+
+function StateMachine:init(states)
+	self.states = states
+	self.current = states["title"]()
+end
+
+function StateMachine:change(state, params)
+	self.current:exit()
+	self.current = self.states[state]()
+	self.current:enter(params)
+end
+function StateMachine:update(dt)
+	self.current:update(dt)
+end
+function StateMachine:render()
+	self.current:render()
+end
