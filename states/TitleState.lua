7bacc024f3d8450593276d76fe8536bbe3f84410
bakr2023-2024 <bakr2023.2024@gmail.com>
2026-05-28 18:32:25 +0300

    refactor: add TitleState class

---
+++ states/TitleState.lua
@@ -0,0 +1,13 @@
+TitleState = Class({ __includes = BaseState })
+function TitleState:update()
+	if love.keyboard.wasPressed("enter") or love.keyboard.wasPressed("return") then
+		gsm:change("play")
+	end
+end
+function TitleState:render()
+	love.graphics.setFont(flappyFont)
+	love.graphics.printf("Fifty Bird", 0, 64, VW, "center")
+
+	love.graphics.setFont(mediumFont)
+	love.graphics.printf("Press Enter", 0, 100, VW, "center")
+end
